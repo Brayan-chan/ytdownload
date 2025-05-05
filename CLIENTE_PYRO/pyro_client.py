6cdac34345fbc1874add3fc597a3741f7303b682
roberstxx <robertomartin942@gmail.com>
2025-05-05 02:22:05 -0600

    Delete CLIENTE_PYRO directory

--- CLIENTE_PYRO/pyro_client.py
+++
@@ -1,8 +0,0 @@
-import Pyro4
-
-Pyro4.config.NS_HOST = "192.168.1.13"  # IP del servidor
-descargador = Pyro4.Proxy("PYRONAME:descargador.youtube")
-
-url = input("Pega la URL del video de YouTube: ")
-resultado = descargador.descargar_video(url)
-print("Resultado:", resultado)
